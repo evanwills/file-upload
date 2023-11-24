38bf652f40bfc2c925525e1004a4a3c4d7edd2de
SFI\EvanW <evan.wills@thesmithfamily.com.au>
2023-11-25 10:46:43 +1100

    finish converting to vue3 composition API

--- src/components/FileUpload/FileUploadPlaceholder.vue
+++ src/components/FileUpload/FileUploadPlaceholder.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg height="100%" viewBox="0 0 210 297" version="1.1" xmlns="http://www.w3.org/2000/svg" :class="stateClass()">
+  <svg height="100%" viewBox="0 0 210 297" version="1.1" xmlns="http://www.w3.org/2000/svg" :class="stateClass">
       <g>
         <g class="placeholder-faux-text-block">
           <rect width="120" height="22" x="28" y="28" />
@@ -49,180 +49,227 @@
       </g>
   </svg>
 </template>
-<script lang="ts">
-import { svgData } from '../../types/FileUpload.d';
+<script lang="ts" setup>
+import { computed, onBeforeMount, ref } from 'vue';
+import type { Ref } from 'vue';
+// import { svgData } from '../../types/FileUpload.d';
 
-export default {
-  props: {
-    fileName: { type: String, required: true },
-    ext: { type: String, required: true },
-    loading: { type: Boolean, required: false, default: false },
-    tooLarge: { type: Boolean, required: false, default: false },
-    isBad: { type: Boolean, required: false, default: false },
-  },
+// --------------------------------------------------
+// START: Properties/attributes
 
-  data() : svgData {
-    return {
-      errorCount: 0,
-      error1a: '',
-      error1b: '',
-      error2a: '',
-      error2b: ''
-    };
-  },
+const props = defineProps({
+  fileName: { type: String, required: true },
+  ext: { type: String, required: true },
+  loading: { type: Boolean, required: false, default: false },
+  tooLarge: { type: Boolean, required: false, default: false },
+  isBad: { type: Boolean, required: false, default: false },
+});
 
-  methods: {
-    stateClass() : string {
-      const tmp = 'file-upload-placeholder'
+//  END:  Properties/attributes
+// --------------------------------------------------
+// START: Local state
 
-      if (this.tooLarge || this.isBad) {
-        return `${tmp} ${tmp}--error`;
-      } else if (this.loading) {
-        return `${tmp} ${tmp}--loading`;
-      } else {
-        return tmp;
-      }
-    }
-  },
+const errorCount : Ref<number> = ref(0);
+const error1a : Ref<string> = ref('');
+const error1b : Ref<string> = ref('');
+const error2a : Ref<string> = ref('');
+const error2b : Ref<string> = ref('');
 
-  mounted() : void {
-    this.errorCount = 0;
+//  END:  Local state
+// --------------------------------------------------
+// START: Local methods
 
-    if (this.isBad) {
-      this.error1a = 'Forbidden file';
-      this.error1b = 'type';
-      this.errorCount += 1;
+const stateClass = computed(() : string => {
+  const tmp = 'file-upload-placeholder'
 
-      if (this.tooLarge) {
-        this.error2a = 'File size is too';
-        this.error2b = 'large';
-        this.errorCount += 1;
-      }
-    } else if (this.tooLarge) {
-      this.error1a = 'File size is too';
-      this.error1b = 'large';
-      this.errorCount += 1;
-    }
+  if (props.tooLarge || props.isBad) {
+    return `${tmp} ${tmp}--error`;
+  } else if (props.loading) {
+    return `${tmp} ${tmp}--loading`;
+  } else {
+    return tmp;
   }
-}
+});
+
+//  END:  Local methods
+// --------------------------------------------------
+// START: Lifecycle methods
+
+onBeforeMount(() : void => {
+  errorCount.value = 0;
+
+  if (props.isBad) {
+    error1a.value = 'Forbidden file';
+    error1b.value = 'type';
+    errorCount.value += 1;
+
+    if (props.tooLarge) {
+      error2a.value = 'File size is too';
+      error2b.value = 'large';
+      errorCount.value += 1;
+    }
+  } else if (props.tooLarge) {
+    error1a.value = 'File size is too';
+    error1b.value = 'large';
+    errorCount.value += 1;
+  }
+});
+
+//  END:  Lifecycle methods
+// --------------------------------------------------
 </script>
 
-<style >
-svg.file-upload-placeholder g {
-  font-family: Verdana, Arial, Helvetica, sans-serif;
-}
-svg.file-upload-placeholder .placeholder-file-meta text {
-  display: block;
-  fill: #000;
-  fill-opacity: 1;
-  font-weight: bold;
-  stroke: none;
-  text-align: center;
-  text-anchor: middle;
-}
+<style lang="scss">
+svg {
+  &.file-upload-placeholder {
+    g {
+      font-family: Verdana, Arial, Helvetica, sans-serif;
+    }
 
-svg.file-upload-placeholder .placeholder-file-meta tspan {
-  font-weight: bold;
-  text-anchor: middle;
-}
-svg.file-upload-placeholder .placeholder-file-name {
-  font-size: 9px;
-  font-family: 'Courier New', monospace;
-}
-svg.file-upload-placeholder .placeholder-ext {
-  font-size: 50px;
-  text-transform: uppercase;
-  filter: drop-shadow(rgba(70, 70, 70, 0.7) -0.5px 1.5px 1px) drop-shadow(rgba(0, 0, 0, 0.5) 3px 3px 3px);
-}
-svg.file-upload-placeholder .placeholder-error-msg text {
-  font-weight: bold;
-  font-size: 60px;
-  font-family: Verdana, Arial, Helvetica, sans-serif;
-  text-align: start;
-  display: inline;
-  fill: #fff;
-  stroke: #000;
-  stroke-width: 1;
-  stroke-opacity: 0.7;
-  stroke-dasharray: none;
-}
+    #placeholder-message-error {
+      fill: #fff;
+      font-size: 90px;
+      stroke: #fff;
+    }
 
-svg.file-upload-placeholder .placeholder-error-msg {
-  display: none;
-}
-svg.file-upload-placeholder .placeholder-error-msg rect {
-  display: inline;
-  fill: #d60000;
-  fill-opacity: 0.7;
-  stroke: none;
-}
+    .placeholder {
+      &-file {
+        &-meta {
+          text {
+            display: block;
+            fill: #000;
+            fill-opacity: 1;
+            font-weight: bold;
+            stroke: none;
+            text-align: center;
+            text-anchor: middle;
+          }
 
-svg.file-upload-placeholder .placeholder-faux-text-block rect {
-  fill: #1a1a1a;
-  fill-opacity: 1;
-  stroke: none;
-}
-svg.file-upload-placeholder #placeholder-message-error {
-  fill: #fff;
-  font-size: 90px;
-  stroke: #fff;
-}
-svg.file-upload-placeholder .placeholder-page path {
-  stroke: #000;
-  stroke-dasharray: none;
-  stroke-opacity: 1;
-}
-svg.file-upload-placeholder .placeholder-page-frame {
-  fill: none;
-  stroke-width: 5;
-}
-svg.file-upload-placeholder .placeholder-page-corner {
-  fill: #000000;
-  stroke-width: 2;
-  fill-opacity: 1;
-}
-svg.file-upload-placeholder .placeholder-page-corner-edge {
-  display:inline;
-  fill:none;
-  stroke:#fff !important;
-  stroke-width:4;
-  stroke-dasharray:none;
-  stroke-opacity:1;
-}
+          tspan {
+            font-weight: bold;
+            text-anchor: middle;
+          }
+        }
 
-svg.file-upload-placeholder .placeholder-loading-indicator {
-  display: none;
-}
-svg.file-upload-placeholder .placeholder-loading-indicator circle {
-  fill: #fff;
-  stroke: none;
-  animation-direction: alternate;
-  animation-duration: 1s;
-  animation-iteration-count: infinite;
-  animation-name: placeholde-slide;
-  animation-timing-function: ease-in-out;
-  /* filter: drop-shadow(rgba(255, 255, 100, 0.5) -1px 0px 3px); */
-}
-svg.file-upload-placeholder .placeholder-loading-indicator circle:nth-of-type(2) { animation-delay: 0.2s; }
-svg.file-upload-placeholder .placeholder-loading-indicator circle:nth-of-type(3) { animation-delay: 0.4s; }
-svg.file-upload-placeholder .placeholder-loading-indicator circle:nth-of-type(4) { animation-delay: 0.6s; }
-svg.file-upload-placeholder .placeholder-loading-indicator text {
-  text-transform: uppercase;
-  fill: #fff;
-  font-size: 50px;
-}
+        &-name {
+          font-size: 9px;
+          font-family: 'Courier New', monospace;
+        }
+      }
 
-svg.file-upload-placeholder--error .placeholder-error-msg {
-  display: block;
-}
-svg.file-upload-placeholder--error .placeholder-laceholder-file-meta text {
-  fill: #fff;
-  stroke: #000;
-  stroke-width: 0.1;
-  stroke-opacity: 0.7;
-}
-svg.file-upload-placeholder--loading .placeholder-loading-indicator {
-  display: inline;
+      &-ext {
+        font-size: 50px;
+        text-transform: uppercase;
+        filter:
+          drop-shadow(rgba(70, 70, 70, 0.7) -0.5px 1.5px 1px)
+          drop-shadow(rgba(0, 0, 0, 0.5) 3px 3px 3px);
+      }
+
+      &-error-msg {
+        display: none;
+
+        text {
+          font-weight: bold;
+          font-size: 60px;
+          font-family: Verdana, Arial, Helvetica, sans-serif;
+          text-align: start;
+          display: inline;
+          fill: #fff;
+          stroke: #000;
+          stroke-width: 1;
+          stroke-opacity: 0.7;
+          stroke-dasharray: none;
+        }
+
+        rect {
+          display: inline;
+          fill: #d60000;
+          fill-opacity: 0.7;
+          stroke: none;
+        }
+      }
+
+      &-faux-text-block {
+        rect {
+          fill: #1a1a1a;
+          fill-opacity: 1;
+          stroke: none;
+        }
+      }
+
+      &-page {
+        path {
+          stroke: #000;
+          stroke-dasharray: none;
+          stroke-opacity: 1;
+        }
+
+        &-frame {
+          fill: none;
+          stroke-width: 5;
+        }
+
+        &-corner {
+          fill: #000000;
+          stroke-width: 2;
+          fill-opacity: 1;
+
+          &-edge {
+            display:inline;
+            fill:none;
+            stroke:#fff !important;
+            stroke-width:4;
+            stroke-dasharray:none;
+            stroke-opacity:1;
+          }
+        }
+      }
+
+      &-loading-indicator {
+        display: none;
+
+        circle {
+          fill: #fff;
+          stroke: none;
+          animation-direction: alternate;
+          animation-duration: 1s;
+          animation-iteration-count: infinite;
+          animation-name: placeholde-slide;
+          animation-timing-function: ease-in-out;
+          /* filter: drop-shadow(rgba(255, 255, 100, 0.5) -1px 0px 3px); */
+
+          &:nth-of-type(2) { animation-delay: 0.2s; }
+          &:nth-of-type(3) { animation-delay: 0.4s; }
+          &:nth-of-type(4) { animation-delay: 0.6s; }
+        }
+
+        text {
+          text-transform: uppercase;
+          fill: #fff;
+          font-size: 50px;
+        }
+      }
+    }
+
+    &--error {
+      .placeholder {
+        &-error-msg {
+          display: block;
+        }
+
+        &-file-meta text {
+          fill: #fff;
+          stroke: #000;
+          stroke-width: 0.1;
+          stroke-opacity: 0.7;
+        }
+      }
+    }
+
+    &--loading .placeholder-loading-indicator {
+      display: inline;
+    }
+  }
 }
 
 @keyframes placeholde-slide {
